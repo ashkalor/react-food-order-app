32853c014153a6f6f7a42059e945ac1679ddc438
AshKalor <akhibkalnoor@gmail.com>
2021-05-18 00:54:23 +0530

    Added functionality to add and remove items from the Cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,14 +1,35 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import { useContext } from "react";
+import CartContext from "../../Store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${Math.abs(cartCtx.totalAmount.toFixed(2))}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const CartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Shushi", amount: "2", price: "12.99" }].map(
-        (item) => {
-          return <li>{item.name}</li>;
-        }
-      )}
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            id={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        );
+      })}
     </ul>
   );
   return (
@@ -16,13 +37,13 @@ const Cart = (props) => {
       {CartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>26.68</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes["button"]}>Order</button>
+        {hasItems && <button className={classes["button"]}>Order</button>}
       </div>
     </Modal>
   );
