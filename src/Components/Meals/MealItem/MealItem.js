2328ab050bd3bd36f0942692a844c81cefbc7689
AshKalor <akhibkalnoor@gmail.com>
2021-05-17 22:36:02 +0530

    Used forward refs and refs to add number of items entered from the MealItemsForm component

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import { useContext } from "react";
+import CartContext from "../../../Store/cart-context";
 
 const MealItems = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.title,
+      price: props.price,
+      amount: amount,
+    });
+  };
+
   return (
     <li className={classes.meal} htmlFor={props.id}>
       <div>
@@ -11,7 +24,7 @@ const MealItems = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
